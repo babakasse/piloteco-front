3cc63f6456dc18253dccaee0563c34e4c9373c99
babakasse <babakasse@yahoo.fr>
2025-07-13 02:33:06 +0200

    peco-27 - dashboard added for connected user

--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -14,8 +14,8 @@ const MaintenanceUnderConstruction = Loadable(lazy(() => import('pages/maintenan
 const MaintenanceComingSoon = Loadable(lazy(() => import('pages/maintenance/coming-soon/coming-soon')));
 
 const AppContactUS = Loadable(lazy(() => import('pages/contact-us')));
-// render - sample page
-const SamplePage = Loadable(lazy(() => import('pages/extra-pages/sample-page')));
+// render - dashboard page
+const Dashboard = Loadable(lazy(() => import('pages/dashboard')));
 
 // ==============================|| MAIN ROUTES ||============================== //
 
@@ -28,11 +28,11 @@ const MainRoutes = {
       children: [
         {
           path: '',
-          element: <Navigate to="/sample-page" replace />
+          element: <Navigate to="/dashboard" replace />
         },
         {
-          path: 'sample-page',
-          element: <SamplePage />
+          path: 'dashboard',
+          element: <Dashboard />
         }
       ]
     },
